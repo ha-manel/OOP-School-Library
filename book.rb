8ce4e5f49d5ea31c84fbf77fcd8d4604829b8624
Manel <manelhammouche3@gmail.com>
2022-07-22 09:11:34 +0100

    Update book class

--- book.rb
+++ book.rb
@@ -1,14 +1,17 @@
 class Book
   attr_accessor :title, :author
 
+  @@books = []
+
   def initialize(title, author)
     @title = title
     @author = author
     @rentals = []
+    @@books << self
   end
 
   def add_rental(person, date)
-    rental = Rental.new(date,self,person)
+    rental = Rental.new(date, self, person)
     @rentals << rental
   end
 end
