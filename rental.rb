dd0b85f0ec8a723d98a4bc958648addf0724222e
ha-manel <manelhammouche3@gmail.com>
2022-07-26 15:11:22 +0100

    Update class

--- rental.rb
+++ rental.rb
@@ -5,7 +5,7 @@ class Rental
   def initialize(date, book, person)
     @date = date
     @book = book
-    @rentee = person
+    @person = person
     person.rentals << self unless person.rentals.include? self
     book.rentals << self unless book.rentals.include? self
   end
